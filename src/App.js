e8ba1d9faac39591e560ae6ed0c8cbcdbf0cd3be
Veronica Pichay <veronicapichay@gmail.com>
2022-12-12 15:41:04 -0800

    use effect implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import MainNav from './components/MainNav';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
+  //API request
+  const [itemData, setItemData] = useState([]);
+
+
+//get some data
+useEffect(() => {
+  const client_id = "qTvoYhj5lwBIculuHLXUAFLgm2X6DaZfPJB6Yxxkn0A"; // Create an account unsplash tp hey client ID;
+  const searchStr = "tech design";
+  const resource = `https://api.unsplash.com/search/photos?query=${searchStr}&per_page=20&client_id=${client_id}`;
+
+  fetch(resource)
     
-    
-    <div className="App">
+    .then((response) => response.json())
+    .then((data) => { 
+      setItemData(data.results);
+      });
+    }, []); 
+
+    return (
+      <div className="App">
         {/* Add a navbar */}
         <MainNav />
         {/* Add card container */}
-        <div className ="card-container">{/* Loop over Card Component */}</div>
+        <div className="card-container">
+          {itemData &&
+            itemData.map((data, index) => (
+              <p key={index}>{data.alt_description}</p>
+            ))}
         </div>
-  );
-}
-
-export default App;
+      </div>
+    );
+  }
+  
+  export default App;
